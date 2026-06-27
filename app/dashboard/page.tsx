5a66d046b22b676f6e08ec587e0181a2e769a884
yusufriduan <yusufriduan@outlook.com>
2026-06-27 12:34:35 +0800

    bug fixes for booking details and resource details

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { BookingListUI } from "../components/BookingListUI";
 import StudentDashboard from "../components/Dashboard/StudentDashboard";
+import ResourceManagerDashboard from "../components/Dashboard/ResourceManagerDashboard";
 
 interface PageProps {
   searchParams: Promise<{ default_sect?: string }>;
@@ -10,7 +11,7 @@ export default async function Dashboard({ searchParams }: PageProps) {
   const resolvedSearchParams = await searchParams;
   const default_sect = resolvedSearchParams.default_sect || null;
 
-  const userRole = 'student';
+  const userRole = 'resourcemanager';
 
   if (userRole === "student") {
     return <StudentDashboard default_sect={default_sect} />;
@@ -19,7 +20,7 @@ export default async function Dashboard({ searchParams }: PageProps) {
     return <div>Staff Dashboard (to be implemented)</div>;
     
   } else if (userRole === "resourcemanager") {
-    return <BookingListUI pageType="list"/>;
+    return <ResourceManagerDashboard default_sect={default_sect} />;
     
   } else {
     return <div className="p-10 text-center text-red-500 font-bold">Unauthorized Role</div>;
